eb00c1dfa92ff5a1d81cb65af3557c25ab833a1e
karthick <karthiram165@gmail.com>
2023-08-28 09:25:29 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `my`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
   ],
